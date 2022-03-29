315b5958a1f127ed90e658da925d25b32899eb45
RedHeadphone <huzaifa.k@somaiya.edu>
2022-03-29 09:59:41 +0530

    added index.js

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+
+import { useEffect } from "react";
+
+import {ethers, Contract} from 'ethers'
+
+
+export default function Home() {
+
+  useEffect(async () => {
+  }
+  );
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+
+    </div >
+  );
+}
